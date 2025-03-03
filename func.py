6418390e6b120113a601ca0892fbfcd12d7c274f
khsatalm <57117932+khsatalm@users.noreply.github.com>
2025-03-03 14:31:26 -0600

    Update func.py

--- func.py
+++ func.py
@@ -1,8 +1,10 @@
 import random
 
+# This is a comment
 def get_random_number():
     return random.randint(1, 100)
 
+#This is another comment
 def get_random_number_between(min_value, max_value):
     return random.randint(min_value, max_value)
 
